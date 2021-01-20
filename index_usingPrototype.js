7cb823dbb09dfd61e260d33b70770820401397d1
lalithesh11 <lalithesh.11@gmail.com>
2021-01-20 20:30:00 +0530

    CollegeLibrary

---
+++ index_usingPrototype.js
@@ -0,0 +1,97 @@
+// Todo's
+// 1. Store all the data to the localStorage
+// 2. Give another column as an option to delete the book
+// 3. Add a scroll bar to the view
+
+
+// Constructor
+function Book(name, author, type) {
+  this.name = name;
+  this.author = author;
+  this.type = type;
+}
+
+// Display Constructor
+function Display() {}
+
+// Add methods to display prototype
+// Adding the form data into the table
+Display.prototype.add = function (book) {
+  console.log("Adding to UI");
+  tableBody = document.getElementById("tableBody");
+  let uiString = `<tr>
+                       <td>${book.name}</td>
+                       <td>${book.author}</td>
+                       <td>${book.type}</td>
+                  </tr>`;
+  tableBody.innerHTML += uiString;
+};
+
+// Implementing the clear function.Clearing the fields after submitting.
+Display.prototype.clear = function () {
+  let libraryForm = document.getElementById("libraryForm");
+  libraryForm.reset();
+};
+
+// Implementing the validate function.
+Display.prototype.validate = function (book) {
+  if (book.name.length < 2 || book.author.length < 2) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+// Implementing the show function
+Display.prototype.show = function (type, alertMessage) {
+  let message = document.getElementById("message");
+
+  message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                       <strong>Message: </strong> ${alertMessage}
+                       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                         <span aria-hidden="true">&times;</span>
+                       </button>
+                     </div>`;
+  //To disable the alert message after 2000millisec or 2sec                  
+  setTimeout(function () {
+    message.innerHTML = "";
+  }, 2000);
+};
+
+// Add submit event listener to libraryForm
+let libraryForm = document.getElementById("libraryForm");
+libraryForm.addEventListener("submit", libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+  console.log("You have submitted library form");
+  let name = document.getElementById("bookName").value;
+  let author = document.getElementById("author").value;
+  let type;
+  let fiction = document.getElementById("fiction");
+  let programming = document.getElementById("programming");
+  let cooking = document.getElementById("cooking");
+
+  if (fiction.checked) {
+    type = fiction.value;
+  } else if (programming.checked) {
+    type = programming.value;
+  } else {
+    type = cooking.value;
+  }
+
+  let book = new Book(name, author, type);
+  console.log(book);
+
+  let display = new Display();
+
+  if (display.validate(book)) {
+    display.add(book);
+    display.clear();
+    display.show("success", "Your book has been successfully added.");
+  } else {
+    //   Show error to the user
+    display.show("danger", "Sorry you cannot add this book");
+  }
+  //The default behaviour of submit is to reload the page. If we dont prevent it , we cannot see the output in console after submitting the form.
+  e.preventDefault();
+}
